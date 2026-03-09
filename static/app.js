c85517006fbed46c8fbe5530a1dc845ea814ea79
claerti <2k2ewyn@gmail.com>
2026-03-09 18:35:03 -0300

    Fixed WSS?

--- static/app.js
+++ static/app.js
@@ -22,9 +22,7 @@ ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 let ws;
 function connect() {
-    // use wss:// for https pages, ws:// for http
-    const protocol = location.protocol === 'https:' ? 'wss:' : 'ws:';
-    ws = new WebSocket(`${protocol}//${location.host}/ws`);
+    ws = new WebSocket(`wss://${location.host}/ws`);
     ws.onopen = () => console.log('connected');
     ws.onmessage = (evt) => {
         const m = JSON.parse(evt.data);
